0dca0b96d119bb6d68a122f4fa59f154bb0c5238
Said Gandur <ferneyvaron0@gmail.com>
2024-04-11 01:37:24 -0500

    add more product components

---
+++ src/app/components/product.tsx
@@ -0,0 +1,64 @@
+import Image from "../../../node_modules/next/image";
+import styles from "../styles/body.module.css";
+import ArrowRightIcon from "../assets/icons/arrowRightIcon";
+import ArrowLeftIcon from "../assets/icons/arrowLeftIcon";
+import ProductDescription from "./productDescription";
+
+export default function Product() {
+  return (
+    <div className={styles.productContainer}>
+      <div className={styles.productImages}>
+        <div className={styles.imageOptions}>
+          <div className={styles.carrouselArrowLeft}>
+            <ArrowLeftIcon />
+          </div>
+          <div className={styles.optionsWrapper}>
+            <Image
+              src="/images/mainImageTv.png"
+              width={128}
+              height={67}
+              alt="Main image Tv"
+            />
+          </div>
+          <div className={styles.optionsWrapper}>
+            <Image
+              src="/images/imageTv2.png"
+              width={128}
+              height={67}
+              alt="Main image Tv"
+            />
+          </div>
+          <div className={styles.optionsWrapper}>
+            <Image
+              src="/images/imageTv3.png"
+              width={128}
+              height={67}
+              alt="Main image Tv"
+            />
+          </div>
+          <div className={styles.optionsWrapper}>
+            <Image
+              src="/images/imageTv4.png"
+              width={128}
+              height={67}
+              alt="Main image Tv"
+            />
+          </div>
+
+          <div className={styles.carrouselArrow}>
+            <ArrowRightIcon />
+          </div>
+        </div>
+        <div className={styles.mainImage}>
+          <Image
+            src="/images/mainImageTv.png"
+            width={470}
+            height={310}
+            alt="Main image Tv"
+          />
+        </div>
+      </div>
+      <ProductDescription />
+    </div>
+  );
+}
